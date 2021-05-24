811f0e7ccc59d86b8d071af57a7a73d76cad8a70
TuanTA <tuanta@vistarsoft.com>
2021-05-24 16:45:42 +0700

    task router

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+        <div className="container">
+          <div className="row">
+              <div className="col-12">
+                  <h1>Day la trang gioi thieu ve chung toi</h1>
+              </div>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default About;
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Router, Route} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+
+const routers = [
+    {
+        name : ""
+    }
+]
+export default routers;
